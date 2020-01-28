99a1fbf6f9035ecb7ef82dc8ce3fcc6af0a7b88f
Dustin Kaban <dustinkaban@gmail.com>
2020-01-28 15:11:21 -0800

    Creating a scene with a tap button

---
+++ IdleFun/Assets/Code/IdleHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleHandler : MonoBehaviour
+{
+    #region SINGLETON SETUP
+    public static IdleHandler Instance = null;
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+    }
+    #endregion
+
+    private void Start()
+    {
+        
+    }
+
+    public void ButtonTapped()
+    {
+
+    }
+}
---
+++ IdleFun/Assets/Code/MainUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUI : MonoBehaviour
+{
+    #region SINGLETON SETUP
+    public static MainUI Instance = null;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+    }
+    #endregion
+    public void TappedButton()
+    {
+        //DO something
+        IdleHandler.Instance.ButtonTapped();
+    }
+}
